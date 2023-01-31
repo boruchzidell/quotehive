ba1b0d15746707865ff56af47af55ec48467c27c
Boruch Zidell <47635324+boruchzidell@users.noreply.github.com>
2023-01-31 16:10:11 -0500

    twilio client working

---
+++ twilio.js
@@ -0,0 +1,13 @@
+const accountSid = 'AC4af4b584d9448a96530003bd48a39e9b';
+const authToken = 'b8ae412e67535b0673a6bd9b4c76bbc3';
+
+const client = require('twilio')(accountSid, authToken);
+
+let counter = 1;
+client.messages
+  .create({
+    body: `Test ${counter++}`,
+    from: '+15166143125',
+    to: '+19173256872',
+  })
+  .then((message) => console.log(message.sid));

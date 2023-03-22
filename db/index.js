5bdd482bc830039b584f2f4a581274bf4ce16d2f
Boruch Zidell <47635324+boruchzidell@users.noreply.github.com>
2023-03-21 19:09:42 -0700

    fix db adapter file to export pg client object

--- db/index.js
+++ db/index.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
-const { Pool } = require('pg');
+const {Pool} = require('pg');
 
-const pool = new Pool(
+const pool = new Pool({
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
   host: process.env.PGHOST,
   port: process.env.PGPORT,
-  database:process.env.PGDATABASE
-);
+  database: process.env.PGDATABASE,
+});
+
+module.exports = {
+  pool,
+};

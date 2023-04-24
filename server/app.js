217017b0bb69d002271fb50c9b1006324636443d
Boruch Zidell <47635324+boruchzidell@users.noreply.github.com>
2023-04-23 22:50:42 -0400

    Introduce cron into app.js

--- server/app.js
+++ server/app.js
@@ -1,7 +1,11 @@
+require('dotenv').config({ path: '../.env' });
+
 let express = require('express');
 let app = express();
+
 let { MessagingResponse } = require('twilio').twiml;
-require('dotenv').config({ path: '../.env' });
+
+let { task: cronTask } = require('./cron');
 
 let path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
@@ -13,11 +17,13 @@ app.post('/sms', (req, res, next) => {
   let smsResponse;
 
   switch (incomingSms) {
-    case 'A':
-      smsResponse = 'One';
+    case 'begin':
+      cronTask.start();
+      smsResponse = 'Cron started';
       break;
-    case 'B':
-      smsResponse = 'Two';
+    case 'enough':
+      cronTask.stop();
+      smsResponse = 'Cron stopped';
       break;
     default:
       smsResponse = 'Not exist';
@@ -31,6 +37,9 @@ app.post('/sms', (req, res, next) => {
   res.send(twiml.toString());
 });
 
+// Uncomment
+// cronTask.start();
+
 let port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`SMS Quotes app listening on port: ${port}`));
+app.listen(port, () => console.log(`Quotehive app listening on port: ${port}`));
--- server/cron.js
+++ server/cron.js
@@ -1,4 +1,18 @@
 let cron = require('node-cron');
 let { getAndSendQuote } = require('./quotes');
 
-cron.schedule('* * * * *', getAndSendQuote, { timezone: 'America/New_york' });
+let minute = '*';
+let hour = '*';
+let monthDate = '*';
+let month = '*';
+let dayOfWeek = '*';
+let cronString = `${minute} ${hour} ${monthDate} ${month} ${dayOfWeek}`;
+
+let task = cron.schedule(cronString, getAndSendQuote, {
+  timezone: 'America/New_york',
+  scheduled: false,
+});
+
+module.exports = {
+  task,
+};
